a2e5a47c5d9366792f2cd2118c1830bfd9452b10
RounakNeogy <56738141+RounakNeogy@users.noreply.github.com>
2020-11-15 21:12:59 +0530

    Add files via upload

---
+++ 2d char array.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char a[][10]={{'a','b','c','\0'},{'d','e','f','\0'}};
+    char b[][10]={"abc","def","pgf"};
+    cout<<a[0]<<" "<<b[1]<<endl;
+}
